6d727ad404fd658beafecd4461606e513b65f5a4
Flair <knguyen20195@yahoo.com>
2025-04-22 00:17:14 -0400

    added repository link

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1 className="text-lg">khang nguyen - ktnguyen@udel.edu</h1>
+      <p>See this repository here: https://github.com/Flair74/181final</p>
+      <p>Progress Doc: https://github.com/Flair74/181final/blob/master/docs/progress.md</p>
     </div>
   );
 }
